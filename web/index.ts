408651bac111fc697d5372327cd5e50c85727f58
Pierre Nel <pierrenel@users.noreply.github.com>
2021-08-31 15:38:35 +0200

    simply svg update

--- web/index.ts
+++ web/index.ts
@@ -145,7 +145,7 @@ const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
-    { text: 'Simply', value: 'https://og-image-orpin-theta.vercel.app/simply.svg' },
+    { text: 'Simply', value: 'https://og-image-nel.vercel.app/simply.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
