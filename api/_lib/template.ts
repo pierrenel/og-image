1bab90d3f716b73932abe92d9f264e0a4d420b6a
Pierre Nel <pierrenel@users.noreply.github.com>
2021-08-31 16:59:39 +0200

    radial

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -12,9 +12,9 @@ const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('b
 const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
-    let background = 'white';
+
     let foreground = 'black';
-    let radial = 'lightgray';
+
 
     if (theme === 'dark') {
         background = 'black';
